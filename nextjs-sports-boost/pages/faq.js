48f7ee239ae097ea3f376c03a9e0866a04213bef
samukapsilva <samukapsilva@gmail.com>
2024-05-05 21:54:34 +0100

    first commit with skeleton

---
+++ nextjs-sports-boost/pages/faq.js
@@ -0,0 +1,8 @@
+export default function FAQPage() {
+    return (
+        <div>
+            <h1>Sports Boost - Páginas de Perguntas FAQ</h1>
+            <a href="/">Ir para a home</a>
+        </div>
+    )
+}
---
+++ nextjs-sports-boost/pages/index.js
@@ -0,0 +1,23 @@
+export default function HomePage() {
+    return (
+        <div>
+          <h1>Sports Boost - Home Page</h1>    
+          <a href="/faq">Ir para o FAQ</a><br></br>
+          <a href="/login">Login</a><br></br>
+          <a href="/login">Escolher Nickname</a><br></br>
+          <a href="/login">Seleção de público/caminho</a><br></br>
+          <h1>Sports Boost - Caminho Fã Investidor</h1> 
+          <a href="/login">Introdução/Onboarding (Tutorial)</a><br></br>
+          <a href="/login">Perfil/XP</a><br></br>
+          <a href="/login">Boost</a><br></br>
+          <a href="/login">Esportes </a><br></br>
+          <a href="/login">Perfil </a><br></br>
+          <a href="/login">NFTS</a><br></br>
+          <a href="/login">Lista de benefícios: </a><br></br>
+          <h1>Sports Boost - Caminho  Atleta</h1> 
+          <a href="/login">Tutorial atleta</a><br></br>
+          <a href="/login">Formulário</a><br></br>
+          <a href="/login">Aguarde o contato da equipe SportsBoost</a><br></br>
+        </div>
+    )   
+}
